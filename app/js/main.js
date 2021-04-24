9733a9914044e7bebca29af0011505257b00f2e2
tima-1701 <tima-1701@mail.ru>
2021-04-24 15:17:41 +0600

    aded

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
-$(function () {
+$(document).ready(function(){
+    $('.desert__inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true
+    });
 
-})
+    var  Mixer  =  mixitup ( '.price__menu', {
+        load: {
+            filter: '.category-a'
+        }
+    });
+    });
+
